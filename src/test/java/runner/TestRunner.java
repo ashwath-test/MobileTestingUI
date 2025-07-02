d78497fd22ab47085299be96a2db20dfabb7bfdb
ashwath-test <39530814+ashwath-test@users.noreply.github.com>
2025-07-02 10:28:29 -0700

    Updated Runner file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,10 @@
 package runner;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.*;
+
 @CucumberOptions(
         features = "src/test/resources/features"
         ,glue={"stepdefinitions","hooks"}
         ,plugin = {"pretty","html:target/cucumber-reports/report.html","json:target/cucumber-report/cucumber.json"}
 )
-
-public class TestRunner {
-
-}
+public class TestRunner extends AbstractTestNGCucumberTests {}
